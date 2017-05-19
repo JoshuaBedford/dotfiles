9e59ce76b4dd22046a244cb6c9d0fbee5c5e7882
Joshua Bedford <joshua@joshuabedford.com>
2017-05-19 16:15:07 -0500

    Making 2 part script
    Due to the addition of installing oh-my-zsh, this must be a 2 part process (until I find a better way).

--- install.sh
+++ install.sh
@@ -19,28 +19,3 @@ chsh -s $(which zsh)
 
 # Install Oh-My-ZSH
 sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
-
-# Install Composer
-curl -sS https://getcomposer.org/installer | php
-mv composer.phar /usr/local/bin/composer
-
-# Install global Composer packages
-/usr/local/bin/composer global require laravel/installer laravel/valet spatie/http-status-check bramus/mixed-content-scan
-
-# Install Laravel Valet
-$HOME/.composer/vendor/bin/valet install
-# cd into ~/Sites
-
-# Install global NPM packages
-npm install --global yarn
-
-# Create a Sites directory
-# This is a default directory for macOS user accounts but doesn't comes pre-installed
-mkdir $HOME/Sites
-cd $HOME/Sites
-$HOME/.composer/vendor/bin/valet park
-cd $HOME/.dotfiles
-
-# Set macOS preferences
-# We will run this last because this will reload the shell
-source .macos
